08e8f0e77a4dc1705011d837d7d205e3f9db5127
JanzenCode <Jordynsmall@icloud.com>
2022-11-07 17:10:11 +1000

    break1

--- src/views/routes.py
+++ src/views/routes.py
@@ -1,3 +1,9 @@
+from controllers.UserController import UserController
+
 @app.route('/', methods=['GET'])
 def index():
     return 'Hello World! TEST'
+
+@app.route('/register/', methods=['POST'])
+def register():
+    return '<p>user added successfully</p>'
