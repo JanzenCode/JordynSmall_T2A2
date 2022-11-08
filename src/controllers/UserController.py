b8ca6f69a3adb81f79cf568e90148de6c3682e62
JanzenCode <Jordynsmall@icloud.com>
2022-11-08 11:08:16 +1000

    debug

--- src/controllers/UserController.py
+++ src/controllers/UserController.py
@@ -1,9 +1,9 @@
-from models.users import User
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
 from sqlalchemy.exc import IntegrityError
+from models.users import User
 
 db = SQLAlchemy()
 ma = Marshmallow()
